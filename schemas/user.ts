d2dfcfaadb4a80edcb316880dad236c2be26b44f
Vishnu Sathyanathan <vishnu@megaexams.com>
2021-01-09 20:25:06 +0530

    Added poll schema

--- schemas/user.ts
+++ schemas/user.ts
@@ -1,19 +1,17 @@
-import mongoose, { Schema ,Document, Model} from 'mongoose'
+import mongoose, { Schema, Document, Model } from 'mongoose';
 
 const User = new Schema({
-	userAgent: String,
-	ipAddress: String,
+  userAgent: String,
+  ipAddress: String,
 });
 
 export interface User {
-	userAgent: string,
-	ipAddress: string
+  userAgent: string;
+  ipAddress: string;
 }
 
-export interface UserDocument extends User, Document{
-}
+export interface UserDocument extends User, Document {}
 
-export interface UserModel extends Model<UserDocument> {
-}
+export interface UserModel extends Model<UserDocument> {}
 
 export default mongoose.model<UserDocument, UserModel>('User', User);
